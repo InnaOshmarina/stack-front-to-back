4ddd06012f2445214057a4925fecc3009965ab5f
Inna <inna.rose2305@gmail.com>
2018-06-17 12:14:09 +0300

    39 - Redux Store, Chrome Extension setup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
     render() {
@@ -11,10 +11,13 @@ class Landing extends Component {
                             <div className="col-md-12 text-center">
                                 <h1 className="display-3 mb-4">Developer Connector
                                 </h1>
-                                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                                <p className="lead">
+                                    {' '}
+                                    Create a developer profile/portfolio, share posts and get help from other developers
+                                </p>
                                 <hr />
-                                <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-                                <a href="login.html" className="btn btn-lg btn-light">Login</a>
+                                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+                                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
                             </div>
                         </div>
                     </div>
@@ -23,7 +26,5 @@ class Landing extends Component {
         );
     }
 }
-
-Landing.propTypes = {};
 
 export default Landing;
