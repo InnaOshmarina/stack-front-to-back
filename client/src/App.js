6bbea46c63bae1aa813f1a718e7d0a3a4b24ef83
Inna <inna.rose2305@gmail.com>
2018-06-22 15:57:08 +0300

    58 - Dashboard Experience Display & Delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import {logoutUser, setCurrentUser} from './actions/authActions';
+import { clearCurrentProfile} from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import './App.css';
 import Navbar from "./components/layout/Navbar";
@@ -13,6 +16,11 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -28,8 +36,8 @@ if(localStorage.jwtToken) {
     if (decoded.exp < currentTime) {
         // Logout user
         store.dispatch(logoutUser());
-        // TODO: Clear current Profile
-
+        // Clear current Profile
+        store.dispatch(clearCurrentProfile());
         // Redirect to login
         window.location.href = '/login';
     }
@@ -46,6 +54,21 @@ class App extends Component {
                   <div className="container">
                       <Route exact path="/register" component={Register} />
                       <Route exact path="/login" component={Login} />
+                      <Switch>
+                        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                      </Switch>
+                      <Switch>
+                          <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                      </Switch>
+                      <Switch>
+                          <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                      </Switch>
+                      <Switch>
+                          <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                      </Switch>
+                      <Switch>
+                          <PrivateRoute exact path="/add-education" component={AddEducation} />
+                      </Switch>
                   </div>
                 <Footer/>
               </div>
